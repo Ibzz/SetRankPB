10d1f4d65b0a72d7cf8319d2f7c62ea18eb807db
PerspectiveDroid <perdroid@ddpctechnologies.co.cc>
2012-03-05 15:47:42 +0100

    Now working with CB-1.1R6

---
+++ src/com/baummann/setrankpb/commands/CommandRank.java
@@ -0,0 +1,118 @@
+package com.baummann.setrankpb.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+import com.baummann.setrankpb.RankHandler;
+import com.baummann.setrankpb.SetRankPB;
+
+public class CommandRank implements CommandExecutor {
+	public static SetRankPB plugin;
+	private RankHandler handler;
+
+	public CommandRank(SetRankPB instance) {
+		plugin = instance;
+		handler = plugin.getHandler();
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdLine, String[] split) {
+		try {
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				if (!plugin.canUse) {
+					player.sendMessage(ChatColor.RED + "---Warning---");
+					player.sendMessage(ChatColor.RED + "This server is not running PermissionsBukkit which makes the plugin unusable.");
+					player.sendMessage(ChatColor.RED + "Please download SuperpermsBridge and PermissionsBukkit to get the plugin to work.");
+					return true;
+				}
+				if (player.hasPermission("setrankpb.rankall") || player.hasPermission("setrankpb.rank." + split[1])) {
+					Player target = plugin.getServer().getPlayer(split[0].toLowerCase());
+					if (player.hasPermission("setrankpb.norank." + plugin.getHandler().getRank(target)) && !player.hasPermission("setrankpb.rankall")) {
+						player.sendMessage(ChatColor.RED + "You may not rank that player!");
+						return true;
+					}
+					if (!target.isOnline()) {
+						if (!plugin.rankOffline) {
+							player.sendMessage(ChatColor.RED + "That player is offline!");
+						}
+						handler.setOfflineRank(split[0], split[1]);
+						player.sendMessage("You've changed " + split[0] + "'s rank to " + split[1] + ".");
+						return true;
+					}
+					handler.setRank(target, split[1]);
+					player.sendMessage("You've changed " + target.getName() + "'s rank to " + split[1] + ".");
+					try {
+						if (target instanceof SpoutPlayer && plugin.achievements && plugin.useSpout) {
+							try {
+								((SpoutPlayer) target).sendNotification("SetRankPB", "You are now " + split[1], Material.getMaterial(plugin.icon.toUpperCase().replace(" ", "_")));
+							} catch (NullPointerException e) {
+								System.out.println("[SetRankPB] WARNING: The material " + plugin.icon.toUpperCase().replace(" ", "_") + " does not exist. Using default.");
+								((SpoutPlayer) target).sendNotification("SetRankPB", "You are now " + split[1], Material.DIAMOND_PICKAXE);
+							} catch (Exception e) {
+							}
+						}
+					} catch (NoClassDefFoundError e) {
+					}
+					return true;
+				} else {
+					player.sendMessage(plugin.noPermission);
+					return true;
+				}
+			} else {
+				if (!plugin.canUse) {
+					sender.sendMessage(ChatColor.RED + "---Warning---");
+					sender.sendMessage(ChatColor.RED + "This server is not running PermissionsBukkit which makes the plugin unusable.");
+					sender.sendMessage(ChatColor.RED + "Please download SuperpermsBridge and PermissionsBukkit to get the plugin to work.");
+					return true;
+				}
+				if (sender.hasPermission("setrankpb.rankall") || sender.hasPermission("setrankpb.rank." + split[1])) {
+					Player target = plugin.getServer().getPlayer(split[0].toLowerCase());
+					if (sender.hasPermission("setrankpb.norank." + plugin.getHandler().getRank(target)) && !sender.hasPermission("setrankpb.rankall")) {
+						sender.sendMessage(ChatColor.RED + "You may not rank that sender!");
+						return true;
+					}
+					if (!target.isOnline()) {
+						if (!plugin.rankOffline) {
+							sender.sendMessage(ChatColor.RED + "That sender is offline!");
+						}
+						handler.setOfflineRank(split[0], split[1]);
+						sender.sendMessage("You've changed " + split[0] + "'s rank to " + split[1] + ".");
+						return true;
+					}
+					handler.setRank(target, split[1]);
+					sender.sendMessage("You've changed " + target.getName() + "'s rank to " + split[1] + ".");
+					try {
+						if (target instanceof SpoutPlayer && plugin.achievements && plugin.useSpout) {
+							try {
+								((SpoutPlayer) target).sendNotification("SetRankPB", "You are now " + split[1], Material.getMaterial(plugin.icon.toUpperCase().replace(" ", "_")));
+							} catch (NullPointerException e) {
+								System.out.println("[SetRankPB] WARNING: The material " + plugin.icon.toUpperCase().replace(" ", "_") + " does not exist. Using default.");
+								((SpoutPlayer) target).sendNotification("SetRankPB", "You are now " + split[1], Material.DIAMOND_PICKAXE);
+							} catch (Exception e) {
+							}
+						}
+					} catch (NoClassDefFoundError e) {
+					}
+					return true;
+				}
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			sender.sendMessage(ChatColor.RED + "Wrong syntax! Usage: /rank [Player] [Rank]");
+			return true;
+		} catch (NullPointerException e) {
+			if (!plugin.rankOffline) {
+				sender.sendMessage(ChatColor.RED + "That player is offline!");
+				return true;
+			}
+			handler.setOfflineRank(split[0], split[1]);
+			sender.sendMessage("You've changed " + split[0] + "'s rank to " + split[1] + ".");
+			return true;
+		}
+		return true;
+	}
+}
